b4cd0e03c9b7d4a224a8bf06ae4a39fad648d8fb
AliE99 <alielahi78@yahoo.com>
2020-07-24 10:32:47 +0430

    MEDIA_ROOT added. note: media_root does the same thing as upload_to in imagefield

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -15,8 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-
-    # image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
