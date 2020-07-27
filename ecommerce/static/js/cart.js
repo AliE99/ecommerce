f117fcc4c8a4b2d6a129545043cc5f0dc1777b77
AliE99 <alielahi78@yahoo.com>
2020-07-27 21:34:32 +0430

    Event handler added

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,1 +1,10 @@
-console.log('Hello World !')
+let updateBtn = document.getElementsByClassName('update-cart');
+
+
+for (let i = 0; i < updateBtn.length; i++) {
+    updateBtn[i].addEventListener('click',function () {
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log('product : ', productId,', action : ',action)
+    })
+}
